b20ccea40a79bb8ef1a4b4190c53ecf829af7c5a
Marcin Kosiba <markosv13@gmail.com>
2020-11-23 22:49:56 +0100

    Create MainMenu.cpp

---
+++ src/views/MainMenu.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Marcin Kosiba on 23.11.2020.
+//
+#include "MainMenu.h"
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+
+using namespace shahu;
+
+bool MainMenu::load()
+{
+    ALLEGRO_BITMAP *tlo = al_load_bitmap("./assets/menu/tlo.png");
+    ALLEGRO_BITMAP *title = al_load_bitmap("./assets/menu/Title.png");
+    ALLEGRO_BITMAP *button1 = al_load_bitmap("./assets/menu/Przycisk_graj.png");
+    ALLEGRO_BITMAP *button2 = al_load_bitmap("./assets/menu/ustawienia.png");
+    ALLEGRO_BITMAP *button3 = al_load_bitmap("./assets/menu/wyjdz.png");
+
+    return true;
+}
+
