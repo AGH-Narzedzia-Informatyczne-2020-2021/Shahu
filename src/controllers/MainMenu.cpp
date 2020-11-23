c952143bef28b3d8dad2e4027a94b25ce962b822
Marcin Kosiba <markosv13@gmail.com>
2020-11-23 22:32:13 +0100

    Update MainMenu.cpp

--- src/controllers/MainMenu.cpp
+++ src/controllers/MainMenu.cpp
@@ -12,13 +12,7 @@ void MainMenu::menu()
     ALLEGRO_BITMAP *button1 = al_load_bitmap("./assets/menu/Przycisk_graj.png");
     ALLEGRO_BITMAP *button2 = al_load_bitmap("./assets/menu/ustawienia.png");
     ALLEGRO_BITMAP *button3 = al_load_bitmap("./assets/menu/wyjdz.png");
-
-    al_draw_bitmap (tlo,0,0,0);
-    al_draw_bitmap (title,0,0,0);
-    al_draw_bitmap (button1,0,0,0);
-    al_draw_bitmap (button2,0,0,0);
-    al_draw_bitmap (button3,0,0,0);
-
+    
 
 }
 
