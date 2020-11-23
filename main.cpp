9c7d80eab598462b8b296187973df7e3594c0f2f
Dawid Słomnicki <d.slomnicki@interia.pl>
2020-11-23 21:30:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
+#include <iostream>
 
 #include "shahu.h"
 
@@ -7,31 +8,81 @@ int main()
 {
     shahu::HelloWorld::printHello();
 
-    al_init();
-    al_install_keyboard();
+    //---------------INITIALIZATION------------
+    if(!al_init())
+    {
+        std::cout << "couldn't initialize allegro" << std::endl;
+        return 1;
+    }
 
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+    if(!al_install_keyboard())
+    {
+        std::cout << "couldn't initialize keyboard\n" << std::endl;
+        return 1;
+    }
+
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
+    if(!timer)
+    {
+        std::cout << "couldn't initialize timer\n" << std::endl;
+        return 1;
+    }
+
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-    ALLEGRO_DISPLAY* disp = al_create_display(320, 200);
+    if(!queue)
+    {
+        std::cout << "couldn't initialize queue\n" << std::endl;
+        return 1;
+    }
+
+    ALLEGRO_DISPLAY* disp = al_create_display(1280, 720);
+    if(!disp)
+    {
+        std::cout << "couldn't initialize display\n" << std::endl;
+        return 1;
+    }
+
     ALLEGRO_FONT* font = al_create_builtin_font();
+    if(!font)
+    {
+        std::cout << "couldn't initialize font\n" << std::endl;
+        return 1;
+    }
 
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
     al_register_event_source(queue, al_get_timer_event_source(timer));
+    //-------------------------
 
+
+
+
+    bool exit = false;
     bool redraw = true;
     ALLEGRO_EVENT event;
 
+    //-------------MAIN LOOP--------------
     al_start_timer(timer);
     while(1)
     {
         al_wait_for_event(queue, &event);
 
-        if(event.type == ALLEGRO_EVENT_TIMER)
-            redraw = true;
-        else if((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
+        switch(event.type)
+        {
+            case ALLEGRO_EVENT_TIMER:
+                redraw = true;
+                break;
+
+            case ALLEGRO_EVENT_KEY_DOWN:
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                exit = true;
+                break;
+        }
+
+        if(exit)
             break;
 
+        //----------Here we go---------------
         if(redraw && al_is_event_queue_empty(queue))
         {
             al_clear_to_color(al_map_rgb(0, 0, 0));
@@ -42,6 +93,7 @@ int main()
         }
     }
 
+    //----------EXIT----------------
     al_destroy_font(font);
     al_destroy_display(disp);
     al_destroy_timer(timer);
