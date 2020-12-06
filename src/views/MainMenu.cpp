8170e0c42025df255f436bd52f618820aab41037
Marcin Kosiba <markosv13@gmail.com>
2020-12-06 13:33:08 +0100

    Update MainMenu.cpp

--- src/views/MainMenu.cpp
+++ src/views/MainMenu.cpp
@@ -7,12 +7,22 @@ using namespace shahu;
 
 bool MainMenu::load()
 {
-    tlo = al_load_bitmap("./assets/menu/tlo.png");
-    title = al_load_bitmap("./assets/menu/Title.png");
-    button1 = al_load_bitmap("./assets/menu/Przycisk_graj.png");
-    button2 = al_load_bitmap("./assets/menu/ustawienia.png");
-    button3 = al_load_bitmap("./assets/menu/wyjdz.png");
+    tlo = al_load_bitmap("../assets/menu/Tlo.png");
+    title = al_load_bitmap("../assets/menu/Title.png");
+    button1 = al_load_bitmap("../assets/menu/Przycisk_graj.png");
+    button2 = al_load_bitmap("../assets/menu/ustawienia.png");
+    button3 = al_load_bitmap("../assets/menu/wyjdz.png");
     //Tak jak Pan Jezus powiedzial
+
+    return true;
+}
+
+bool MainMenu::render() {
+    al_draw_bitmap(tlo,0,0,0);
+    al_draw_bitmap(title,0,0,0);
+    al_draw_bitmap(button1,x[0],y[0],0);
+    al_draw_bitmap(button2,x[1],y[1],0);
+    al_draw_bitmap(button3,x[2],y[2],0);
 
     return true;
 }
