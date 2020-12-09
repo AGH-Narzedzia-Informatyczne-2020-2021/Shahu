2d4e496d68a7c55dc14c3625f750a6110d3393ca
Marcin Kosiba <markosv13@gmail.com>
2020-12-09 20:51:53 +0100

    Update MainMenu.cpp

--- src/views/MainMenu.cpp
+++ src/views/MainMenu.cpp
@@ -27,3 +27,19 @@ bool MainMenu::render() {
     return true;
 }
 
+void MainMenu::click(int a, int b, ALLEGRO_BITMAP **b1, ALLEGRO_BITMAP **b2, ALLEGRO_BITMAP **b3){
+    if((x[0]<=a<=x[0]+300)&&y[0]<=b<=y[0]+150){
+        *b1 = al_load_bitmap("../assets/menu/Przycisk_graj_wcisniety.png");
+        //render();
+    }
+    if((x[1]<=a&&a<=x[1]+130)&&y[1]<=b&&b<=y[1]+145){
+        *b2 = al_load_bitmap("../assets/menu/ustawienia_wcisniete.png");
+        //render();
+    }
+    if(x[2]<=a&&a<=x[2]+300&&y[2]<=b&&b<=y[2]+150){
+        *b3 = al_load_bitmap("../assets/menu/wyjdz_wcisniety.png");
+        //render();
+    }
+
+}
+
