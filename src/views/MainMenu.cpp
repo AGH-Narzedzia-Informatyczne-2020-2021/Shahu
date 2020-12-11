f2355454d61a8eb4d0fd15f06d41e8f16bc83a7f
Marcin Kosiba <markosv13@gmail.com>
2020-12-11 13:55:02 +0100

    Update MainMenu.cpp

--- src/views/MainMenu.cpp
+++ src/views/MainMenu.cpp
@@ -3,6 +3,7 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_image.h>
 
+
 using namespace shahu;
 
 bool MainMenu::load()
@@ -26,16 +27,21 @@ bool MainMenu::render() {
     return true;
 }
 
-void MainMenu::click(float a, float b){
+bool MainMenu::click(float a, float b){
     if((x[0]<=a&&a<=x[0]+300)&&(y[0]<=b&&b<=y[0]+150)){
         button1 = al_load_bitmap("../assets/menu/Przycisk_graj_wcisniety.png");
-
+        return false;
     }
     if((x[1]<=a&&a<=x[1]+130)&&(y[1]<=b&&b<=y[1]+145)){
         button2 = al_load_bitmap("../assets/menu/ustawienia_wcisniete.png");
+        return false;
     }
     if(x[2]<=a&&a<=x[2]+300&&y[2]<=b&&b<=y[2]+150){
-        button3 = al_load_bitmap("../assets/menu/wyjdz_wcisniety.png");
+        button3 = al_load_bitmap("../assets/menu/wyjdz_wcisniete.png");
+        al_draw_bitmap(button3,x[2],y[2],0);
+        al_flip_display();
+        _sleep(3);
+        return true;
     }
 
 }
